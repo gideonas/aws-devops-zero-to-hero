dd40a55e1edcd69ff9b054bc5320f795abffc1c6
gideonas <166926250+gideonas@users.noreply.github.com>
2024-05-07 01:23:45 +0200

    Update start_container.sh
    port change to 500

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull  ramgid/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000  ramgid/simple-python-flask-app
+docker run -d -p 500:5000  ramgid/simple-python-flask-app
